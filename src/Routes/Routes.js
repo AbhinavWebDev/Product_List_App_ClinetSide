9994866798a0fa8f76716dc26136b18630de6db9
Abhinav A <abhinav.kky@gmail.com>
2022-11-15 18:59:20 +0530

    env File removed

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from '../Pages/Home/Home'
 import { AddCategory } from '../Pages/AddCategory/AddCategory';
 import { AddProduct } from '../Pages/AddProduct/AddProduct';
 import { ProductList } from '../Pages/ProductList/ProductList';
@@ -11,8 +10,8 @@ export const RouteManagement = () => {
         <>
             <Routes>
                
-                <Route path="/" element={<Home />} />
-                <Route path="/addcategory" element={ <AddCategory />} />
+              
+                <Route path="/" element={ <AddCategory />} />
                 <Route path="/addproduct" element={ <AddProduct />} />
                 <Route path="/productlist" element={ <ProductList />} />
             </Routes>
